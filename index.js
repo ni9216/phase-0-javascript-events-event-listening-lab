e35c10b12cd43dabce11fc302edc6bc550c09cea
ni9216 <nicholasliech851@.com>
2024-07-23 15:39:39 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+    // index.js
+
+function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
+}
+
+// Call the function to set up the event listener
+addingEventListener();
+
 }
